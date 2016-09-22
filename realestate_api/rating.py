dbfb0d28405086be3af238c65f9618c656b710c9
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-09-22 05:12:08 +0200

    Added simple rating and other options possibility

--- realestate_api/rating.py
+++ realestate_api/rating.py
@@ -2,11 +2,12 @@ import requests
 import json
 import random
 
+import pdb
 
 
-data = open("./data", "r")
+#data = open("./data", "r")
 
-data = data.read()
+#data = data.read()
 
 
 
@@ -14,9 +15,14 @@ data = data.read()
 def rate(houses):
 
 #Property type, room_number,size_min,size_max, price_min, price_max 
+    print(houses)
+    to_rent = list(filter(lambda x: x["listing_type"] == "rent", houses))
+    houses = list(filter(lambda x: x["listing_type"] == "buy", houses))
 
-    to_rent = filter(lambda x: x["listing_type"] == "rent", houses)
-    houses = sorted(houses, key = lambda x: (x["price_high"], x["size"]))
+
+    print(len(houses))
+
+    houses = sorted(houses, key = lambda x: (x["ppsm"],x["price"], x["size"]))
 
 
     #Calculate average rent
@@ -35,7 +41,43 @@ def rate(houses):
     #Return top 5
     return houses[0:5]
 
-def other_options():
+
+def format_response(d):
+
+    d["avg_user_queries"] = random.randrange(30, 400)
+    d["avg_add_expiration"] = random.randrange(1,30)
+    d["rent_trend_3months"] = random.randrange(-1,2)
+    d["new_house_offerings_trend"] = random.randrange(-1,2)
+
+    for num in range(0 ,len(d["listings"])):
+        fake = d["listings"][num]
+        if fake["construction_year"] == 0:
+            fake["construction_year"] = random.randrange(1990, 2016)
+        if fake["img_url"] == "http://resources.nestimg.com/nestoria/img/cs4.2_v1.png":
+            fake["img_url"] = "http://t2.gstatic.com/images?q=tbn:ANd9GcRliOf6pVeiyHjwS2_BN3sdKh_ak3VEQ4d_AfPdF6gFSYk9nKKu8qSO"
+        if fake["thumb_url"] == "http://resources.nestimg.com/nestoria/img/cs4.2_v1.png":
+            fake["thumb_url"] = "http://t2.gstatic.com/images?q=tbn:ANd9GcRliOf6pVeiyHjwS2_BN3sdKh_ak3VEQ4d_AfPdF6gFSYk9nKKu8qSO"
+        if fake["listing_type"] == "buy":
+            if random.randrange(0,2) > 0:
+                fake["listing_type"] = "buy"
+            else:
+                fake["listing_type"] = "rent"
+                fake["price"] = fake["price"] / 500
+                fake["price_low"] = fake["price"]
+                fake["price_high"] = fake["price"]
+        if fake["size"] == 0:
+            if fake["property_type"] == "flat":
+                fake["size"] = random.randrange(70, 120)
+            else:
+                fake["size"] = random.randrange(90, 180)
+        fake["ppsm"] = fake["price"] / fake["size"]
+        d["listings"][num] = fake
+    return d
+
+
+
+
+def other_options(params):
 
     ne = (53.561577, 14.046648)
     sw = (47.784556, 7.858369)
@@ -47,23 +89,24 @@ def other_options():
 
         neapi = (round((ne[0] - sw[0])*random.random() + sw[0], 6), round((ne[1] - sw[1])*random.random() + sw[1], 6))
         swapi = (round((neapi[0] - sw[0])*random.random() + sw[0], 6), round((neapi[1] - sw[1])*random.random(), 6))
-        params = {
 
-            "action" : "search_listings",
-            "encoding" : "json", 
-            "south_west" : "%f,%f" %(swapi[0], swapi[1]),
-            "north_east" : "%f,%f" %(neapi[0], neapi[1])
-        }
+        params["south_west"] = "%f,%f" %(swapi[0], swapi[1])
+        params["north_east"] = "%f,%f"  %(neapi[0], neapi[1])
 
 
         #Call to the astoria API
         response = requests.get("http://api.nestoria.de/api?", params = params)
         d = json.loads(response.text)
+
+        d = format_response(d["response"])
+
         #Extend with top 5 of this group
-        other.extend(rate(d["response"]["listings"]))
+        other.extend(rate(d["listings"]))
 
     print(other)  
     return other
 
-other_options()
-#rate(json.loads(data)["response"]["listings"])
+#d = format_response(json.loads(data))
+#print(rate(d["response"]["listings"]))
+
+
--- realestate_api/views.py
+++ realestate_api/views.py
@@ -2,6 +2,8 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from realestate_api import rating as r
+
 
 from django.core.cache import cache
 from django.shortcuts import render
@@ -10,6 +12,7 @@ from django.shortcuts import render
 import requests
 import json
 import random
+import pdb
 
 
 MAPS_API_KEY = 'AIzaSyDBs7baI6Lq4B2A5sDtVVoFyhPOP6VBXLY'
@@ -27,20 +30,21 @@ def get_realestates(request, format=None):
 
     }
 
-
     for key, value in request.GET.items():
         params[key] = value
 
 
     #Call to the astoria API
     response = requests.get("http://api.nestoria.de/api?", params = params)
+    
+    #Analyse the data
+    d = json.loads(response.text)
+    dd = r.format_response(d["response"])
 
+    top_5 = r.rate(dd["listings"])
+    top_other = r.other_options(params)
 
-    #TODO filter the results based on how good they are
-
-
-    listings_info = format_response(response)
-
+    d["other_options"] = top_other
     return Response(d)
 
 
@@ -68,37 +72,3 @@ def get_streetview(request, format=None):
 def get_map(request):
 
     return render(request, "map.html", {})
-
-
-def format_response(response):
-
-    d = json.loads(response.text)
-
-
-    d["response"]["avg_user_queries"] = random.randrange(30, 400)
-    d["response"]["avg_add_expiration"] = random.randrange(1,30)
-    d["response"]["rent_trend_3months"] = random.randrange(-1,2)
-    d["response"]["new_house_offerings_trend"] = random.randrange(-1,2)
-
-    for fake in d["response"]["listings"]:
-        if fake["construction_year"] == 0:
-            fake["construction_year"] = random.randrange(1990, 2016)
-        if fake["img_url"] == "http://resources.nestimg.com/nestoria/img/cs4.2_v1.png":
-            fake["img_url"] = "http://t2.gstatic.com/images?q=tbn:ANd9GcRliOf6pVeiyHjwS2_BN3sdKh_ak3VEQ4d_AfPdF6gFSYk9nKKu8qSO"
-        if fake["thumb_url"] == "http://resources.nestimg.com/nestoria/img/cs4.2_v1.png":
-            fake["thumb_url"] = "http://t2.gstatic.com/images?q=tbn:ANd9GcRliOf6pVeiyHjwS2_BN3sdKh_ak3VEQ4d_AfPdF6gFSYk9nKKu8qSO"
-        if fake["listing_type"] == "buy":
-            if random.randrange(0,2) > 0:
-                fake["listing_type"] = "buy"
-            else:
-                fake["listing_type"] = "rent"
-                fake["price"] = fake["price"] / 500
-                fake["price_low"] = fake["price"]
-                fake["price_high"] = fake["price"]
-        if fake["size"] == 0:
-            if fake["property_type"] == "flat":
-                fake["size"] = random.randrange(70, 120)
-            else:
-                fake["size"] = random.randrange(90, 180)
-
-    return d
